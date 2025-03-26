ac4b41a1ef9f0b411f92702c78ae3818b11337ea
GabrielVinicius-1 <gv1717177@gmail.com>
2025-03-26 14:03:40 +0000

    Criei o Cadastro da Carta 1 Criei o Cadastro da Carta 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,5 +2,82 @@
 
 int main() {
    printf("Desafio Super Trunfo!\n");
+
+    char estado;
+
+    char codigo[3];
+
+    char cidade[30];
+
+    int populacao;
+
+    float area, pib;
+
+    int turismo;
+
+    // Cadastro da Carta 1 //
+
+    printf("Carta1\n");
+
+    printf("Insira o estado:\n");
+    scanf("%c", &estado);
+    printf("Estado: %c\n", estado);
+
+    printf("Insira o codigo:\n");
+    scanf("%s", &codigo);
+    printf("Codigo: %s\n", codigo);
+
+    printf("Insira a cidade:\n");
+    scanf("%s", &cidade);
+    printf("Cidade: %s\n", cidade);
+
+    printf("Insira a populacao:\n");
+    scanf("%d", &populacao);
+    printf("Populacao: %d\n", populacao);
+
+    printf("Insira a area:\n");
+    scanf("%f", &area);
+    printf("Area: %.2f KM²\n", area);
+
+    printf("Insira o PIB:\n");
+    scanf("%f", &pib);
+    printf("PIB; %.2f bilhoes de reais\n", pib);
+
+    printf("Insira o numero de pontos turisticos:\n");
+    scanf("%d", &turismo);
+    printf("Pontos Turisticos: %d\n", turismo);
+
+    // Cadastro da Carta 2 //
+
+    printf("Carta2\n");
+
+    printf("Insira o estado:\n");
+    scanf("%c", &estado);
+    printf("Estado: %c\n", estado);
+
+    printf("Insira o codigo:\n");
+    scanf("%s", &codigo);
+    printf("Codigo: %s\n", codigo);
+
+    printf("Insira a cidade:\n");
+    scanf("%s", &cidade);
+    printf("Cidade: %s\n", cidade);
+
+    printf("Insira a populacao:\n");
+    scanf("%d", &populacao);
+    printf("Populacao: %d\n", populacao);
+
+    printf("Insira a area:\n");
+    scanf("%f", &area);
+    printf("Area: %.2f KM²\n", area);
+
+    printf("Insira o PIB:\n");
+    scanf("%f", &pib);
+    printf("PIB; %.2f bilhoes de reais\n", pib);
+
+    printf("Insira o numero de pontos turisticos:\n");
+    scanf("%d", &"pontosturisticos");
+    printf("Pontos Turisticos: %d\n", "pontosturisticos");
+
     return 0;
 }
